2b72b8e3774ce3d2362229b783861ab254e783d1
Valeriy Petrenko <fourcher00@gmail.com>
2025-12-30 16:26:36 -0500

    Minor fix for GithubPage

--- js/api/api.js
+++ js/api/api.js
@@ -1,16 +1,18 @@
-const PRODUCTS_URL = "../../data/db.json";
+import { BASE_URL } from "../config.js";
+
+const PRODUCTS_URL = `${BASE_URL}data/db.json`;
 
 export async function getProducts() {
   try {
     const res = await fetch(PRODUCTS_URL);
-    console.log("fetched fine");
+
     if (!res.ok) {
       throw new Error(`Failed to fetch products`);
     }
 
     const data = await res.json();
-    console.log(data);
-    return Array.isArray(data.products) ? data.products : [];
+
+    return data.products;
   } catch (err) {
     console.error("Error while fetching products:", err);
     return []; // return empty array to prevent crashes
--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
 export const IS_PROD = location.hostname !== "localhost";
+export const BASE_URL =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/view-glasses-shop/";
+export const BASE_PATH = import.meta.env?.BASE_URL || "/view-glasses-shop/";
